5720e59cc3a89ebee479b23e425ecac302b69370
Abdulakh Kurbanov <143484012+Leguna1@users.noreply.github.com>
2024-03-09 19:02:56 +0100

    WindowClassCheck

--- main.cpp
+++ main.cpp
@@ -1,41 +1,33 @@
 #include <SFML/Graphics.hpp>
+#include "Window.h"
 
+int main() {
+    Window window("Bouncing Mushroom", sf::Vector2u(640, 480));
 
-void main(int argc, char** argv[]) {
-	sf::RenderWindow window(sf::VideoMode(640, 480),
-		"Bouncing mushroom.");
-	sf::Texture mushroomTexture;
-	mushroomTexture.loadFromFile("Mushroom.png");
-	sf::Sprite mushroom(mushroomTexture);
-	sf::Vector2u size = mushroomTexture.getSize();
-	mushroom.setOrigin(size.x / 2, size.y / 2);
-	sf::Vector2f increment(0.4f, 0.4f);
-	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
-		}
-		if ((mushroom.getPosition().x + (size.x / 2) >
-			window.getSize().x && increment.x > 0) ||
-			(mushroom.getPosition().x - (size.x / 2) < 0 &&
-				increment.x < 0))
-		{
-			// Reverse the direction on X axis.
-			increment.x = -increment.x;
-		}
-		if ((mushroom.getPosition().y + (size.y / 2) >
-			window.getSize().y && increment.y > 0) ||
-			(mushroom.getPosition().y - (size.y / 2) < 0 &&
-				increment.y < 0))
-		{
-			// Reverse the direction on Y axis.
-			increment.y = -increment.y;
-		}
-		mushroom.setPosition(mushroom.getPosition() + increment);
-		window.clear(sf::Color(16, 16, 16, 255)); // Dark gray.
-		window.draw(mushroom); // Drawing our sprite.
-		window.display();
-	}
+    sf::Texture mushroomTexture;
+    mushroomTexture.loadFromFile("Mushroom.png");
+    sf::Sprite mushroom(mushroomTexture);
+    sf::Vector2u size = mushroomTexture.getSize();
+    mushroom.setOrigin(size.x / 2, size.y / 2);
+    sf::Vector2f increment(0.4f, 0.4f);
+
+    while (!window.IsDone()) {
+        window.Update();
+
+        if ((mushroom.getPosition().x + (size.x / 2) > window.GetWindowSize().x && increment.x > 0) ||
+            (mushroom.getPosition().x - (size.x / 2) < 0 && increment.x < 0)) {
+            increment.x = -increment.x;
+        }
+        if ((mushroom.getPosition().y + (size.y / 2) > window.GetWindowSize().y && increment.y > 0) ||
+            (mushroom.getPosition().y - (size.y / 2) < 0 && increment.y < 0)) {
+            increment.y = -increment.y;
+        }
+        mushroom.setPosition(mushroom.getPosition() + increment);
+
+        window.BeginDraw();
+        window.Draw(mushroom);
+        window.EndDraw();
+    }
+
+    return 0;
 }
